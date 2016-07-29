8a6d3768e5a51f13c003b281d22810cf1dbaaa9b
sajid <sajid@Sajid-Dayer.(none)>
2016-07-29 22:58:57 +0530

    ajax calling successfully start

--- arya_lib/urls.py
+++ arya_lib/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import url
 from arya_lib import views
 #from django.conf.urls import patterns
 urlpatterns = [
-url(r'^$', views.login, name='login'),
+url(r'^$', views.login_page, name='login_page'),
+url(r'home',views.home_page,name='home_page'),
 ]
--- arya_lib/views.py
+++ arya_lib/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
-def login(request):
+from django.http import HttpResponse
+def login_page(request):
 	return render(request, 'arya_lib/log in.html')
+def home_page(request):
+	print request.GET.get('username')
+	return HttpResponse("response is done")
+def login(request):
+	return view_login(request)
+
